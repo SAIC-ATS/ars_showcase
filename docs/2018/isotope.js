cecaf3d13c74e1f22577cc137287dbd8de8705fc
Doug Rosman <drosman@saic.edu>
2021-05-10 02:59:48 -0500

    update 2018

--- docs/2018/isotope.js
+++ docs/2018/isotope.js
@@ -1,5 +1,11 @@
-
-    
-    
-    
-   
+let $container = $('#grid-container').imagesLoaded(function(){
+  $container.isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer',
+      gutter: 10
+      // fitWidth: true
+    }
+  });
+});
--- docs/2018/main.js
+++ docs/2018/main.js
@@ -1,21 +1,37 @@
-$(document).ready(function(){
-  
-    for(let i = 0; i < 30; i++) {
+generateContent();
 
-        if(Math.random() < 0.5) {
-            let newGridItem = `<img src="images/portrait/${i+1}.png" alt="portrait_${i+1}" class="grid-item">`
-            $('#cluster1').append(newGridItem);
-        } else {
-            let newGridItem = `<img src="images/landscape/${i+1}.png" alt="portrait_${i+1}" class="grid-item">`
-            $('#cluster1').append(newGridItem);
-        }
-       
+function generateContent(){
+
+  for(let i = 0; i < 30; i++) {
+
+    if(i == 0) {
+      let newGridItem = `<img src="images/portrait/${i+1}.png" alt="portrait_${i+1}" class="grid-item grid-sizer">`
+      $('#grid-container').append(newGridItem);
+    } else {
+      if(Math.random() < 0.5) {
+        let newGridItem = `<img src="images/portrait/${i+1}.png" alt="portrait_${i+1}" class="grid-item">`
+        $('#grid-container').append(newGridItem);
+      } else {
+        let newGridItem = `<img src="images/landscape/${i+1}.png" alt="portrait_${i+1}" class="grid-item">`
+        $('#grid-container').append(newGridItem);
+      }
     }
-    // $('.isotope-grid').isotope({
-    //     itemSelector: '.grid-item',
-    //     masonry: {
-    //     columnWidth: 100
-    //     }
-    // });  
-})
+  }
+
+}
+
+
+// $('.isotope-grid').isotope({
+//     itemSelector: '.grid-item',
+//     masonry: {
+//     columnWidth: 100
+//     }
+// });  
+
+
+
+
+
+
+
 
