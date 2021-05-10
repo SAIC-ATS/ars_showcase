ce5e73403a6adeb548985ac5c68afb6444e1b551
Doug Rosman <drosman@saic.edu>
2021-05-10 03:15:35 -0500

    more responsive

--- docs/2018/isotope.js
+++ docs/2018/isotope.js
@@ -1,11 +1,11 @@
 let $container = $('#grid-container').imagesLoaded(function(){
   $container.isotope({
     itemSelector: '.grid-item',
-    percentPosition: true,
+    // percentPosition: true,
     masonry: {
-      columnWidth: '.grid-sizer',
-      gutter: 10
-      // fitWidth: true
+      columnWidth: 100,
+      gutter: 5,
+      fitWidth: true
     }
   });
 });
