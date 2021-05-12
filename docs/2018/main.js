3aca6e53652a0f1eca140d3874b7784e646cdc97
Doug Rosman <drosman@saic.edu>
2021-05-12 11:42:24 -0500

    add lightbox

--- docs/2018/main.js
+++ docs/2018/main.js
@@ -1,16 +1,38 @@
-createGallery();
+createLightboxGallery();
 
-function createGallery() {
+// function createGallery() {
+//   for(let i = 0; i < 27; i++) {
+//     let newGridItem = `
+//     <div class="grid-item">
+//       <img src="images/thumbs/${i+1}.jpg" alt="ars2018_${i+1}" class="grid-img">
+//     </div>`
+//     $('#grid-container').append(newGridItem);
+//   }
+// }
+
+function createLightboxGallery() {
   for(let i = 0; i < 27; i++) {
     let newGridItem = `
     <div class="grid-item">
-      <img src="images/thumbs/${i+1}.jpg" alt="ars2018_${i+1}">
+      <a href="images/fullsize/${i+1}.jpg" data-lightbox="2018" data-title="caption" data-alt="alt-text">
+        <img src="images/thumbs/${i+1}.jpg" alt="ars2018_${i+1}" class="grid-img"
+      </a>
     </div>`
     $('#grid-container').append(newGridItem);
   }
 }
 
-////////// LIGHTBOX
+lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true,
+  'alwaysShowNavOnTouchDevices': true,
+  'disableScrolling': true,
+  'fadeDuration': 600,
+  'imageFadeDuration': 600,
+  'showImageNumberLabel': false,
+  'positionFromTop': 100,
+})
+
 
 
 ///////// SCROLL TO TOP
