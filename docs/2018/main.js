7041f022b17046ce0c7b8c3f6a15d25b54385893
Doug Rosman <drosman@saic.edu>
2021-05-12 01:30:32 -0500

    looking good

--- docs/2018/main.js
+++ docs/2018/main.js
@@ -1,9 +1,6 @@
-// generateContent();
-// generateDivs();
-generateWrappedContent();
+createGallery();
 
-function generateWrappedContent(){
-
+function createGallery() {
   for(let i = 0; i < 27; i++) {
     let newGridItem = `
     <div class="grid-item">
@@ -13,8 +10,12 @@ function generateWrappedContent(){
   }
 }
 
-$(window).scroll(function() {
+////////// LIGHTBOX
 
+
+///////// SCROLL TO TOP
+
+$(window).scroll(function() {
   if($(window).scrollTop() > 800) {
     $('.back-to-top').addClass('show');
   } else {
@@ -22,58 +23,12 @@ $(window).scroll(function() {
   }
 });
 
-$('#back-to-top').on('click', function(e) {
-  // When the user clicks on the button, scroll to the top of the document
+$('.back-to-top').on('click', function(e) {
   e.preventDefault();
     $('html, body').animate({
         scrollTop: 0
-    }, 700);
+    }, '500');
 });
-
-
-function generateContent(){
-
-  for(let i = 0; i < 27; i++) {
-    let newGridItem = `<img src="images/ars_images_2018/0${i+1}.jpg" alt="ars2018${i+1}" class="grid-item">`
-    $('#grid-container').append(newGridItem);
-  }
-}
-
-function generateDivs(){
-
-  for(let i = 0; i < 27; i++) {
-    let newGridItem = `<div class="grid-item"></div>`
-    $('#grid-container').append(newGridItem);
-  }
-}
-
-
-function generateRandomContent(){
-
-  for(let i = 0; i < 30; i++) {
-
-    if(i == 0) {
-      let newGridItem = `<img src="images/portrait/${i+1}.png" alt="portrait_${i+1}" class="grid-item grid-sizer">`
-      $('#grid-container').append(newGridItem);
-    } else {
-      if(Math.random() < 0.5) {
-        let newGridItem = `<img src="images/portrait/${i+1}.png" alt="portrait_${i+1}" class="grid-item">`
-        $('#grid-container').append(newGridItem);
-      } else {
-        let newGridItem = `<img src="images/landscape/${i+1}.png" alt="portrait_${i+1}" class="grid-item">`
-        $('#grid-container').append(newGridItem);
-      }
-    }
-  }
-}
-
-
-// $('.isotope-grid').isotope({
-//     itemSelector: '.grid-item',
-//     masonry: {
-//     columnWidth: 100
-//     }
-// });  
 
 
 
