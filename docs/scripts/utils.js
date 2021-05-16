9374ea89f2a65ed4c1e122d21288c45d13739b82
Doug Rosman <drosman@saic.edu>
2021-05-16 13:50:40 -0500

    redo top navbar

--- docs/scripts/utils.js
+++ docs/scripts/utils.js
@@ -3,6 +3,13 @@ $('.topnav__right-hamburger').click(function(){
     $('.topnav__dropdown-menu-container').slideToggle(400);
 });
 
+$(window).resize(function(){
+
+  if($(window).width() > 950 && $('.topnav__dropdown-menu-container').css('display') == 'block') {
+    $('.topnav__dropdown-menu-container').slideUp(200);
+  }
+})
+
   
 ///////// SCROLL TO TOP
 $(window).scroll(function() {
